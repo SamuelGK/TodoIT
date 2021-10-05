ac8ea21bcf283579ae819a1b8125e7073c56a455
SamuelGK <89768420+SamuelGK@users.noreply.github.com>
2021-10-05 10:52:43 +0200

    Add project files.

---
+++ Models/Person.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoIT.Models
+{
+    public class Person
+    {
+        readonly int personId;
+
+        string firstName;
+        string lastName;
+        
+        public int Personid
+        {
+            get { return personId; }
+        }
+
+        public string Firstname
+        {
+            get { return firstName; } 
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException();
+                }
+                firstName = value;
+
+
+            }
+        }
+
+        public string Lastname
+        {
+            get { return lastName; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException();
+                }
+                lastName = value;
+            }
+
+        } 
+
+        public string Fullname
+        {
+            get
+            {
+                return firstName + " " + lastName;
+            }
+
+        } 
+
+        public Person(int id, string firstName, string lastName  ) 
+        {
+            personId = id;
+            Firstname = firstName;
+            Lastname = lastName;
+        }
+
+    } // end of person 
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using TodoIT.Models;
+
+namespace TodoIT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Todo IT");
+            Person person = new Person(5, "Geralt", "Of Rivia");
+
+            Console.WriteLine(person.Fullname);
+
+
+
+        }
+
+        
+    }
+}
