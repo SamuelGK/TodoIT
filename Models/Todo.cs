95870c934a1ddb931fcded1baef1952aa3b9cac8
SamuelGK <89768420+SamuelGK@users.noreply.github.com>
2021-10-12 08:43:09 +0200

    edited

---
+++ Models/Todo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoIT.Models
+{
+    public class Todo
+    {
+        Person vem;
+        string beskrivning;
+        int id = 3;
+        bool klar;
+        
+
+    }
+
+    
+}
